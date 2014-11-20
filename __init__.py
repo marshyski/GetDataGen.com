45d0b33364c4f628e5b2fef1df8b92abd76960e2
marshyski <timski@linux.com>
2014-11-20 15:18:10 -0500

    Cleaned up DB creation

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ app.config['ALLOWED_EXTENSIONS'] = set(['xml'])
 # Create dummy secrey key so we can use sessions
 app.config['SECRET_KEY'] = '123456790'
 # Create in-memory database
-app.config['DATABASE_FILE'] = 'sample_db.sqlite'
+app.config['DATABASE_FILE'] = 'simple_db.sqlite'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
@@ -224,26 +224,6 @@ def build_sample_db():
     # test_user = User(login="test", password="test")
     test_user = User(login="test", password=generate_password_hash("test"))
     db.session.add(test_user)
-
-    first_names = [
-        'Harry', 'Amelia', 'Oliver', 'Jack', 'Isabella', 'Charlie','Sophie', 'Mia',
-        'Jacob', 'Thomas', 'Emily', 'Lily', 'Ava', 'Isla', 'Alfie', 'Olivia', 'Jessica',
-        'Riley', 'William', 'James', 'Geoffrey', 'Lisa', 'Benjamin', 'Stacey', 'Lucy'
-    ]
-    last_names = [
-        'Brown', 'Smith', 'Patel', 'Jones', 'Williams', 'Johnson', 'Taylor', 'Thomas',
-        'Roberts', 'Khan', 'Lewis', 'Jackson', 'Clarke', 'James', 'Phillips', 'Wilson',
-        'Ali', 'Mason', 'Mitchell', 'Rose', 'Davis', 'Davies', 'Rodriguez', 'Cox', 'Alexander'
-    ]
-
-    for i in range(len(first_names)):
-        user = User()
-        user.first_name = first_names[i]
-        user.last_name = last_names[i]
-        user.login = user.first_name.lower()
-        user.email = user.login + "@example.com"
-        user.password = generate_password_hash(''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(10)))
-        db.session.add(user)
 
     db.session.commit()
     return
